53456dd41789334ce87a3bfe5b0268a70f2561fe
Attila Večerek <avecerek@zendesk.com>
2019-06-18 10:03:56 +0200

    Add failing test

--- src/App.test.js
+++ src/App.test.js
@@ -7,3 +7,7 @@ it('renders without crashing', () => {
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
+
+it('makes this test fail', () => {
+  expect(1 * 1).toBe(2);
+});
